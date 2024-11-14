e4b31eca9e6042f87e058c56b1ea772769d3d68d
Hoangnguyenhao <tranhoangnguyenhao2003@gmail.com>
2024-11-14 13:22:10 +0700

    Delete Lab6.JP2 directory

--- Lab6.JP2/General/IFileService.java
+++
@@ -1,6 +0,0 @@
-package General;
-import java.util.List;
-public interface IFileService<T> {
-    public  List<T> readFileStatistics(String fileInPath);
-    public List<T> writeFileStatistics(String fileOutPath);
-}
